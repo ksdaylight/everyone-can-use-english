9b259ffe90e0daafd1de72de7fbb9c7c51851db6
ksdaylight <yuanmiwen111@163.com>
2025-03-23 11:44:55 +0800

    feat: change playMode

--- enjoy/src/renderer/components/medias/media-bottom-panel/media-player-controls.tsx
+++ enjoy/src/renderer/components/medias/media-bottom-panel/media-player-controls.tsx
@@ -58,7 +58,7 @@ export const MediaPlayerControls = () => {
   } = useContext(MediaShadowProviderContext);
   const { EnjoyApp } = useContext(AppSettingsProviderContext);
   const { currentHotkeys } = useContext(HotKeysSettingsProviderContext);
-  const [playMode, setPlayMode] = useState<"loop" | "single" | "all">("all");
+  const [playMode, setPlayMode] = useState<"loop" | "single" | "all">("single");
   const [playbackRate, setPlaybackRate] = useState<number>(1);
   const [grouping, setGrouping] = useState(false);
 
